dfd0d352e6516ec30bdac07aa6c3c35354cdb531
socialclimber <newtnnkeng@gmail.com>
2018-12-04 14:07:17 +0200

    Using dispartcher to call servlets

---
+++ RequestDispatcherExample/src/com/socialtech/ArithmeticServlet.java
@@ -0,0 +1,38 @@
+package com.socialtech;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ArithmeticServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		double i = Double.parseDouble(req.getParameter("num1"));
+		String operator = req.getParameter("operator");
+		double j = Double.parseDouble(req.getParameter("num2"));
+		RequestDispatcher rd;
+		if(operator.equals("+")) {
+			rd = req.getRequestDispatcher("add");
+			rd.forward(req, res);
+		}else if(operator.equals("-")){
+			rd = req.getRequestDispatcher("subtract");
+			rd.forward(req, res);
+		}else if(operator.equals("*")) {
+			rd = req.getRequestDispatcher("multiply");
+			rd.forward(req, res);
+		}else if(operator.equals("/")) {
+			rd = req.getRequestDispatcher("division");
+			rd.forward(req, res);
+		}else if(operator.equals("%")) {
+			rd = req.getRequestDispatcher("modulus");
+			rd.forward(req, res);
+		}else {
+			res.getWriter().println("Invalid operator!");
+		}
+		
+		
+	}
+}
---
+++ RequestDispatcherExample/src/com/socialtech/DivisionServlet.java
@@ -0,0 +1,19 @@
+package com.socialtech;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DivisionServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		double i = Integer.parseInt(req.getParameter("num1"));
+		double j = Integer.parseInt(req.getParameter("num2"));
+		double result = i+j;
+		PrintWriter out = res.getWriter();
+		out.println("Result of "+i+" / "+j+" is: "+result);
+	}
+
+}
